08c3d9dbba7299871e637af829c2fc223dcb0bf1
Max Isaev <ismak89@gmail.com>
2020-06-01 12:13:43 +0300

    Adjusted XHR middleware

--- api/middleware/xhr.go
+++ api/middleware/xhr.go
@@ -10,8 +10,11 @@ const customAppHeader = "x-request-client"
 
 var XhrMiddleware = func(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method == "OPTIONS" {
+			return
+		}
 		appHeader := r.Header.Get(customAppHeader)
-		if appHeader == "" && r.Method != "OPTIONS" {
+		if appHeader == "" {
 			log.Printf("Unauthorized request: missing '%s' header\n", customAppHeader)
 			u.RespondJson(w, u.Response{Message: "Missing authentication data", ErrorCode: u.ErrAuth}, http.StatusForbidden)
 			return
